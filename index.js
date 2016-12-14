1871e1b32c3f4895e0902836bffb7b3bcaab0824
Matt Walters <mattwalters5@gmail.com>
2016-12-14 02:16:59 -0500

    adding git clone and git status

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports.register = (program) => {
 
   program
-    .command('git', 'manage your meta repo and child git repositories');
+    .command('git', 'manage your meta repo and child git repositories')
+    .parse(process.argv);
 
   require('./bin/meta-git');
 
