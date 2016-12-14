2dc55502b374d1353768e7c1f43a543804c281c1
Matt Walters <mattwalters5@gmail.com>
2016-12-14 11:52:12 -0500

    fixing double command registration and double fire issue. bumping loop-things for dynamic cross-env location discovery

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ module.exports.register = (program) => {
 
   program
     .command('git', 'manage your meta repo and child git repositories')
-    .parse(process.argv);
-
-  require('./bin/meta-git');
+    // .parse(process.argv);
 
 }
