24b2c36c930dd12a1d6487acd781571431890e73
anchnk <anachronik@gmail.com>
2018-06-13 12:18:16 +0200

    style(white-space): remove trailing whitespaces
    This patch remove trailing white spaces in all JavaScript sources.

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 module.exports.register = (program) => {
-
   program
     .command('git', 'manage your meta repo and child git repositories')
-
 }
 
 module.exports.update = (options) => {
-
   require('./lib/metaGitUpdate')(options);
-  
 }
