bbfc4b212096296fa7ee5cd89b7a593db917d02c
ojizero <z.acnts.816@pm.me>
2020-02-06 21:00:42 +0200

    feat: pass through all subcommands to Git direcly

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 exports.register = program => {
   program.command('git', 'manage your meta repo and child git repositories');
 };
-
-exports.update = options => {
-  require('./lib/metaGitUpdate')(options);
-};
